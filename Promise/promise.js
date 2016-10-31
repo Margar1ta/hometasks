79fe1663ebc9ffcb446d33f53fc58c882c1fd40b
Margar1ta <naruyaoi@yandex.ru>
2016-10-31 13:40:36 +0300

    Update promise.js

--- Promise/promise.js
+++ Promise/promise.js
@@ -1,1 +1,12 @@
 
+function Promise(s)
+{
+return new MyPromise(function(resolve,reject){
+var xhr = new XMLHttpRequest();
+             
+  		xhr.open('POST', 'http://kodaktor.ru/api/req?name=ilva&sleep='+s, true);
+  		xhr.responseType = 'json';  
+
+})
+
+}
